f0bd47d81ea49b85b493d90cd082896676842e08
sabamiso <s.kondo11235813@gmail.com>
2024-06-04 00:57:30 +0900

    テスト

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ site_dict = dict()
 with open("./urlList.json", encoding="utf-8") as f:
     site_dict = json.load(f)
 
+with open("/home/runner/firebase_credentials.json", "r") as f:
+    print(f.readall())
+
+print(site_dict)
+
 ws_machine = MinistrySiteDataGetter()
 ret = ws_machine.update_all_data(site_dict)
 
