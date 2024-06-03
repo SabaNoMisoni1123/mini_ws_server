ca276f279340ba40757d9b7dad86f86517d75f61
sabamiso <s.kondo11235813@gmail.com>
2024-06-04 00:30:04 +0900

    jsonファイルの取り扱いを変更

--- wslib.py
+++ wslib.py
@@ -21,25 +21,9 @@ class MinistrySiteDataGetter:
             "mlitNews": funclib.get_mlit_news,
             "mlitIndividualNews": funclib.get_mlit_individual_news,
         }
-        print(os.getenv("FIREBASE_TYPE"))
+        print(os.getenv("FIREBASE_CRED_FNAME"))
 
-        service_account_info = {
-            "type": os.getenv("FIREBASE_TYPE"),
-            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-            "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv(
-                "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
-            ),
-            "client_x509_cert_url": os.getenv("FIREBAS_CLIENT_X509_CERT_URL"),
-            "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
-        }
-
-        cred = credentials.Certificate(service_account_info)
+        cred = credentials.Certificate(os.getenv("FIREBASE_CRED_FNAME"))
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
