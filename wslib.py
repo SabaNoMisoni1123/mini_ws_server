56db0ccee542a2b6ea53fe806e9a95584ed105aa
Sota Kondo <sota_kondo@mri.co.jp>
2024-06-11 21:33:21 +0900

    安定版

--- wslib.py
+++ wslib.py
@@ -22,23 +22,23 @@ class MinistrySiteDataGetter:
             "mlitIndividualNews": funclib.get_mlit_individual_news,
         }
 
-        service_account_info = {
-            "type": os.getenv("FIREBASE_TYPE"),
-            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-            "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-            "auth_provider_x509_cert_url": os.getenv(
-                "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
-            ),
-            "client_x509_cert_url": os.getenv("FIREBAS_CLIENT_X509_CERT_URL"),
-            "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
-        }
+        # service_account_info = {
+        #     "type": os.getenv("FIREBASE_TYPE"),
+        #     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        #     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        #     "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+        #     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        #     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        #     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        #     "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        #     "auth_provider_x509_cert_url": os.getenv(
+        #         "FIREBASE_AUTH_PROVIDER_X509_CERT_URL"
+        #     ),
+        #     "client_x509_cert_url": os.getenv("FIREBAS_CLIENT_X509_CERT_URL"),
+        #     "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+        # }
 
-        cred = credentials.Certificate(service_account_info)
+        cred = credentials.Certificate("ws-db-11235813-firebase-adminsdk-lh4mi-50c38e64b5.json")
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
